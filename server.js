99f1ce30f09c03824d1cb2fde718a1309d6c525e
DimaALabsi <dema.yaser8@gmail.com>
2022-04-03 16:31:43 +0300

    add cors

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 "use strict";
 const express = require("express");
+const cors = require("cors");
 const notFoundHandler = require("./handlers/404");
 const errorHandler = require("./handlers/500");
 
+
 const app = express();
+
 const customerRouter = require("./routes/customer");
 const orderRouter = require("./routes/order");
 const itemRouter = require("./routes/items");
 app.use(express.json());
-
+app.use(cors())
 app.use(customerRouter);
 app.use(orderRouter);
 app.use(itemRouter);
